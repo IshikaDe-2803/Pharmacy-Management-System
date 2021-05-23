3ca5233f0d5a7485ecbcbfd48ec51d9e6d5df7ac
IshikaDe-2803 <dmishika2002@gmail.com>
2021-05-23 23:07:38 +0530

    second commit

--- Med_invent1.py
+++ Med_invent1.py
@@ -1,127 +1,199 @@
+import datetime #datetime module --> to check validity of date
 from tabulate import tabulate #tabulate module--> display table in proper format
-import mysql.connector #connectingm ysql with python
-mydb = mysql.connector.connect(
+import mysql.connector #connecting mysql with python
+
+mydb = mysql.connector.connect( #establishing connection with MySQL database
   host="localhost",
   user="root",
   password="Rootpass123*",
   database="medical_inventory",
 )
 mycursor = mydb.cursor() #creating mycursor object to access database elements
+
 #class med defines the medical inventory
 class med:
-    def __init__(medi, ref_no,comp_name,med_type,m_name,m_qnt,m_cost):
+    def __init__(medi, ref_no,comp_name,med_type,m_name,m_qnt,m_cost): #__init__ initializes class variables
         medi.ref_no=ref_no
         medi.comp_name=comp_name
         medi.med_type=med_type
         medi.m_name=m_name
         medi.m_qnt=m_qnt
         medi.m_cost=m_cost
+
     #add() inserts medicinal records into the inventory
     def add(medi):
         #insertion of values into the inventory
         ins_medin = "INSERT INTO med_inv (ref_no,comp_name,med_type,m_name,m_qnt,m_cost) VALUES (%s,%s,%s,%s,%s,%s)"
         val_medin= (medi.ref_no,medi.comp_name,medi.med_type,medi.m_name,medi.m_qnt,medi.m_cost)
-        mycursor.execute(ins_medin,val_medin)
-        mydb.commit()
+        mycursor.execute(ins_medin,val_medin)#executes the MySQL commands
+        mydb.commit()#sends a COMMIT statement to the MySQL server, committing the current transaction
         print("\nMedicine record successfully inserted.")
+
+#class bills defines the generation of bill section
 class bills:
-    def __init__(c, cust_name,cust_contact,cust_date,cust_address,cust_docref):
+    def __init__(c, cust_name,cust_contact,cust_date,cust_address,cust_docref):#__init__ initializes class variables
         c.cust_name=cust_name
         c.cust_contact=cust_contact
         c.cust_date=cust_date
         c.cust_address=cust_address
         c.cust_docref=cust_docref
-    def bill_ch(c):
+
+    def bill_ch(c):#bill_ch() takes user-choice to add/delete bill records/generate a bill
         print("\n1.Add medicine to cart\n2.Delete medicine from cart\n3.Generate bill\n")
         ch2=int(input("Enter choice: "))
         if(ch2==1):
-            c.add_b()
+            c.add_b()#Add medicine to bill
             print("\nMedicine successfully added.")
             c.bill_ch()
         elif(ch2==2):
-            del_b()
+            del_b()#delete medicine from bill
             c.bill_ch()
         elif(ch2==3):
-            c.disp_b()
+            c.disp_b()#display the bill
             mainpg()
         else:
             print("\nInvalid choice.")
             c.bill_ch()
-    def add_b(c):
+
+    def add_b(c):#add a medicinal record in the bill
         m_n=input("Enter the medicine name- ")
-        mycursor.execute("SELECT EXISTS(SELECT m_name from med_inv where m_name=%s)",(m_n,))
+        mycursor.execute("SELECT m_name from med_inv where m_name=%s",(m_n,))
         myresult3=mycursor.fetchone()
         mycursor.execute("SELECT m_qnt from med_inv where m_name=%s",(m_n,))
         quant=mycursor.fetchone()
-        if((myresult3[0]==0) and (quant[0]>0) ): #checking availability of medicine
-            print("Medicine not available.")
+        mycursor.execute("SELECT m_cost from med_inv where m_name=%s",(m_n,))
+        m_cst=mycursor.fetchone()#fetching the cost of medicine
+        m_q=int(input("Enter the medicine quantity- "))
+        if(not myresult3 or quant[0]==0):
+            print("Medicine entered is not available.")
             c.add_b()
-        else:
-            mycursor.execute("SELECT m_cost from med_inv where m_name=%s",(m_n,))
-            m_cst=mycursor.fetchone()
-            m_q=int(input("Enter the medicine quantity- "))
-            #update inventory
-            mycursor.execute("UPDATE med_inv set m_qnt=m_qnt-%s where m_name=%s",(m_q,m_n,))
-            ins_bil = "INSERT INTO bill (cust_date,cust_contact,med_name,med_qnt,med_cost,med_total) VALUES (%s,%s,%s,%s,%s,%s)"
-            val_bil= (c.cust_date,c.cust_contact,m_n,m_q,m_cst[0],m_cst[0]*m_q)
-            mycursor.execute(ins_bil,val_bil)
-            mydb.commit()
-            print("Customer record added.")
-    def disp_b(c):
+        elif(m_q>quant[0]):
+            print("Insufficient medicines in inventory. Medicine not available.")
+            c.add_b()
+        #update inventory-quantity of medicine
+        mycursor.execute("UPDATE med_inv set m_qnt=m_qnt-%s where m_name=%s",(m_q,m_n,))
+        ins_bil = "INSERT INTO bill (cust_date,cust_contact,med_name,med_qnt,med_cost,med_total) VALUES (%s,%s,%s,%s,%s,%s)"
+        val_bil= (c.cust_date,c.cust_contact,m_n,m_q,m_cst[0],m_cst[0]*m_q)
+        mycursor.execute(ins_bil,val_bil)
+        mydb.commit()
+        print("Customer record added.")
+
+    def disp_b(c):#displaying the bill
+        #displaying customer details
         print("Customer Name: ", c.cust_name)
         print("Customer Contact: ", c.cust_contact)
         print("Date of purchase: ",c.cust_date)
         print("Customer Address: ", c.cust_address)
         print("Doctor referral: ", c.cust_docref)
         mycursor.execute("SELECT med_name,med_qnt,med_cost,med_total FROM bill where cust_contact=%s and cust_date=%s",(c.cust_contact,c.cust_date,))
-        myresult1 = mycursor.fetchall() 
+        myresult1 = mycursor.fetchall() #Displaying  bill details
         print(tabulate(myresult1,headers = ["Medicine Name","Quantity","Cost per item","Total Cost"], tablefmt='psql',numalign="left"))
         ins_cust= "SELECT SUM(med_total) from bill where cust_contact= %s and cust_date=%s "
         mycursor.execute(ins_cust,(c.cust_contact,c.cust_date))
         myresult2 = mycursor.fetchone()
-        print("Total payable:", myresult2[0])
+        print("Total payable:", myresult2[0])#displaying total payable
+        #updating bill into bill history table
         ins_bilhist = "INSERT INTO bill_hist (cust_name,cust_contact,cust_date,cust_address,cust_docref, total) VALUES (%s,%s,%s,%s,%s,%s)"
         val_bilhist= (c.cust_name,c.cust_contact,c.cust_date,c.cust_address,c.cust_docref,myresult2[0])
         mycursor.execute(ins_bilhist,val_bilhist)
         mydb.commit()
+
 #dele() is used to delete records from the inventory
 def dele():
-    del_refno=int(input("\nEnter the record reference number that you want to delete:"))
-    del_medin = "DELETE FROM med_inv WHERE ref_no = %s"
-    mycursor.execute(del_medin,(del_refno,))
-    mydb.commit()
-    print("\nMedicine record was successfully deleted.")
+    del_refno=int(input("Enter the record reference number that you want to delete:"))
+    mycursor.execute("SELECT ref_no FROM med_inv WHERE ref_no=%s",(del_refno,))
+    ref_present=mycursor.fetchone() 
+    if(not ref_present):#checking presence of record
+        print("\nRecord not present.")
+    else:        
+        del_medin = "DELETE FROM med_inv WHERE ref_no = %s" #deleting the record via MySQL query
+        mycursor.execute(del_medin,(del_refno,))
+        mydb.commit()
+        print("\nMedicine record was successfully deleted.")
+        
 #disp() is used to display the inventory of pharmacy
 def disp():
     #Selecting data from inventory
     mycursor.execute("SELECT * FROM med_inv")
     myresult = mycursor.fetchall()
+    #displaying inventory in tabular form
     print(tabulate(myresult,headers = ["Ref No: ","Company Name","Medicine Type","Medicine Name","Quantity","Cost"], tablefmt='psql',numalign="left"))
+
 #del_b() func deletes the med_name record of customer
 def del_b():
     del_medname=input("\nEnter the medicine name that you want to delete:")
-    del_med = "DELETE FROM bill WHERE med_name = %s "
-    mycursor.execute(del_med,(del_medname,))
-    mydb.commit()
-    print("\nMedicine record was successfully deleted.")
+    mycursor.execute("SELECT med_name FROM bill WHERE med_name=%s",(del_medname,))
+    med_present=mycursor.fetchone()
+    if(not med_present):#checking presence of record
+        print("\nRecord not present.")
+    else:
+        mycursor.execute("SELECT med_qnt FROM bill WHERE med_name=%s",(del_medname,))
+        med_qnt=mycursor.fetchone()
+        mycursor.execute("UPDATE med_inv set m_qnt=m_qnt+%s where m_name=%s",(med_qnt[0],del_medname,))#updating the quantity changes in inventory
+        del_med = "DELETE FROM bill WHERE med_name = %s "
+        mycursor.execute(del_med,(del_medname,))
+        mydb.commit()
+        print("\nMedicine record was successfully deleted.")
+
 #displaying customer details
-def findbill():
-    find_contact=input("Enter contact of customer: ")
-    find_date=input("Enter date (DD-MM-YYYY) of bill: ")
+def findbill(): 
+    ValidNO=False
+    while(ValidNO==False):#checking validity of contact number
+        find_contact=input("Enter contact of customer: ")
+        if (len(find_contact)==10 and find_contact.isnumeric()):
+              
+            ValidNO=True
+        else :
+            print ("Invalid Contact Number entered.")
+            ValidNO=False
+    ValidDate=False
+    while(ValidDate==False):#checking validity of date
+        find_date=input("Enter date (DD/MM/YYYY) of bill: ")
+        if(isValidDate(find_date)==False):
+            ValidDate=False
+            print("Invalid Date entered.\n")
+        else:
+            ValidDate=True   
     mycursor.execute("SELECT * from bill_hist where cust_date=%s and cust_contact=%s",(find_date,find_contact,))
-    find_cust=mycursor.fetchall()
-    print("\nCUSTOMER DETAILS: \n")
-    print(tabulate(find_cust,headers = ["Customer Name","Contact","Purchase Date","Address","Reference","Total Amt"], tablefmt='psql',numalign="left"))
+    find_cust=mycursor.fetchall()#customer details in find_cust
     mycursor.execute("SELECT med_name,med_qnt,med_cost,med_total from bill where cust_date=%s and cust_contact=%s",(find_date,find_contact,))
-    find_meds=mycursor.fetchall()
-    print("\nMEDICINE PURCHASE DETAILS: \n")
-    print(tabulate(find_meds,headers = ["Medicine Name","Qnt.","Cost per item","Total Cost"], tablefmt='psql',numalign="left"))
+    find_meds=mycursor.fetchall()#medicine details in find_cust
+    if(not find_cust or not find_meds):#checking presence of records
+        print("\nCustomer/Medicinal record not found!")
+    else:
+        print("\nCUSTOMER DETAILS: \n")#Displaying the bill and customer details
+        print(tabulate(find_cust,headers = ["Customer Name","Contact","Purchase Date","Address","Reference","Total Amt"], tablefmt='psql',numalign="left"))
+        print("\nMEDICINE PURCHASE DETAILS: \n")
+        print(tabulate(find_meds,headers = ["Medicine Name","Qnt.","Cost per item","Total Cost"], tablefmt='psql',numalign="left"))
+
 #display sale history through sale_hist()
 def sale_hist():
-    d=input("Enter the date (DD-MM-YYYY) for which you want to view sale history:")
-    mycursor.execute("SELECT cust_name,cust_contact,total from bill_hist WHERE cust_date=%s",(d,))
-    sale=mycursor.fetchall()
-    print(tabulate(sale,headers = ["Customer Name","Contact","Total Amt"], tablefmt='psql',numalign="left"))
+    d=input("Enter the date (DD/MM/YYYY) for which you want to view sale history:")
+    if(isValidDate(d)==True):#check validity of date
+        mycursor.execute("SELECT cust_name,cust_contact,total from bill_hist WHERE cust_date=%s",(d,)) 
+        sale=mycursor.fetchall()
+        if(not sale):#check if records are present
+            print("\nRecords for the date ",d," not found!!")
+        else:#printing particulars of the sale history
+            print(tabulate(sale,headers = ["Customer Name","Contact","Total Amt"], tablefmt='psql',numalign="left"))
+    else:
+        print("Invalid Date entered.\n")
+        sale_hist()
+
+#isValidDate checks validity of date
+def isValidDate(inputDate):
+    try:
+        day,month,year = inputDate.split('/')
+    except ValueError :
+        return False
+    else:
+        try :
+            datetime.datetime(int(year),int(month),int(day))
+        except ValueError :
+            return False
+        else:
+            return True
+
 #mainpg() function is the first page that presents the functions user may want to use
 def mainpg():
     print("\n**************************")
@@ -129,7 +201,7 @@ def mainpg():
     print("**************************\n")
     #displaying options of functions that user may want to perform
     print("1. Display Inventory\n2. Add medicine to inventory\n3. Delete Medicine from inventory\n4. Generate Bill\n5. Find bill\n6. View Sale History\n7. Exit")
-    choice1=int(input("\nEnter the function you want to perform:"))
+    choice1=int(input("\nEnter the function you want to perform:"))#Allows user to perform respective functions
     print("\n")
     if(choice1==1):
         #Display inventory
@@ -137,6 +209,7 @@ def mainpg():
         mainpg()
     elif(choice1==2):
         #Add a medicine to inventory
+        #Accepting medicine details
         ref_no=input("Enter medicine reference number: ")
         comp_name=input("Enter medicine company name: ")
         med_type=input("Enter medicine type: ")
@@ -144,26 +217,43 @@ def mainpg():
         m_qnt=input("Enter medicine quantity: ")
         m_cost=input("Enter medicine cost per unit: ")   
         m1=med(ref_no,comp_name,med_type,m_name,m_qnt,m_cost)
-        m1.add()
+        m1.add()#medicine details passed to add() function
         mainpg()
     elif(choice1==3):
         #Delete a medicine from Inventory
         dele()
         mainpg()
     elif(choice1==4):
+        #Customer details input accepted
         cust_name=input("Enter customer name: ")
-        cust_contact=input("Enter customer contact: ")
-        cust_date=input("Enter date of purchase: ")
+        
+        ValidNO=False
+        while(ValidNO==False):
+            cust_contact=input("Enter customer contact: ")
+            if (len(cust_contact)==10 and cust_contact.isnumeric()):#Check validity of phone number
+                   
+                ValidNO=True
+            else :
+                print ("Invalid Contact Number entered.")
+                ValidNO=False
+        ValidDate=False
+        while(ValidDate==False):#Check validity of date
+            cust_date=input("Enter date (DD/MM/YYYY) of purchase: ")
+            if(isValidDate(cust_date)==False):
+                print("Invalid Date entered.\n")
+                ValidDate=False
+            else:
+                ValidDate=True
         cust_address=input("Enter customer address: ")
         cust_docref=input("Enter customer's doctor referal: ")
         b1=bills(cust_name,cust_contact,cust_date,cust_address,cust_docref)
-        b1.bill_ch()
+        b1.bill_ch()#Customer details passed to bill_ch() to add/del/generate bill
     elif(choice1==5):
         #find/display a particular customer's bill on particular date
         findbill()
         mainpg()
     elif(choice1==6):
-        #display sale history
+        #display sale history of a particular date
         sale_hist()
         mainpg()
     elif(choice1==7):
@@ -174,22 +264,24 @@ def mainpg():
         print("Invalid choice.")
         mainpg()
 
-def login():
+def login():#login fn 
     print("***********************************************************")
     print("WELCOME TO HEALTH24 PHARMACY".ljust(10)) 
     print("***********************************************************\n")
     print("LOGIN TO SYSTEM:\n")
-    username= input("Enter username(employee I-D): ")
-    pswrd= input("Enter password: ")
-    mycursor.execute("SELECT * from login")
+    username= input("Enter username(employee I-D): ")#Accepting username
+    pswrd= input("Enter password: ")#Accepting password
+    mycursor.execute("SELECT * from login")#select query 
     check=mycursor.fetchall()    #check has tuples within tuples  
-    if ((any(username in i for i in check))and (any(pswrd in i for i in check))) : #check the occurrence of username and password
+    if ((any(username in i for i in check))and (any(pswrd in i for i in check))) : #check the occurrence of username and password in table
         print("Login Successful!!\n")
     else :
         print("!!Invalid Credentials. Access denied!!")
         print("___________________________________________________________\n")
-        login()
+        login()#allows relogin incase of invalid credentials
+
 #call of login() to begin execution
 login()
+
 #call of mainpg() to proceed of execution
 mainpg()
--- Med_invforproj.sql
+++ Med_invforproj.sql
@@ -1,5 +1,6 @@
 create database medical_inventory;
 use medical_inventory;
+
 create table med_inv(
 ref_no integer,
 comp_name char(40),
@@ -9,13 +10,7 @@ m_qnt integer,
 m_cost decimal
 );
 select * from med_inv;
-create table bill(
-med_name char(40),
-med_cost decimal,
-sr_no integer,
-cust_contact char(40) PRIMARY KEY
-);
-drop table bill;
+delete from med_inv;
 create table bill(
 cust_date char(15),
 cust_contact char(40) ,
@@ -25,6 +20,7 @@ med_cost decimal,
 med_total decimal
 );
 select* from bill;
+
 create table login(
 emp_id char(40) ,
 pswrd char(40) 
@@ -33,6 +29,7 @@ insert into login values("EM120","abcd*");
 insert into login values("EM100","#log");
 insert into login values("EM90","emp@123");
 select* from login;
+
 create table bill_hist(
 cust_name char(40),
 cust_contact char(20),
@@ -42,4 +39,7 @@ cust_docref char(20),
 total decimal
 );
 select* from bill_hist;
-delete from bill where cust_date="02-05-2021";
+
+delete from bill;
+delete from bill_hist;
+
